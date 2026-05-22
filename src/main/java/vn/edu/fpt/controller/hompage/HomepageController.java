f0cef82a41c1d70a18df2a587ee09f3b9ba63488
TuanNPA040905 <tuannpahe190953@gmail.com>
2026-05-22 14:48:44 +0700

    "Cấu hình kết nối đến CSDL và tạm thời disable cái Spring security đi (cho đỡ bắt đăng nhập)"

---
+++ src/main/java/vn/edu/fpt/controller/hompage/HomepageController.java
@@ -0,0 +1,15 @@
+package vn.edu.fpt.controller.hompage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomepageController {
+
+    @GetMapping("/")
+    public String homepage(){
+        return "Nguyễn Phúc Anh Tuấn";
+    }
+}
