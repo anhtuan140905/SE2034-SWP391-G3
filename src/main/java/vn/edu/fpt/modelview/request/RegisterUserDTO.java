108850a22e2ec0e21304302d0287dc4d36f0dd59
TuanNPA040905 <tuannpahe190953@gmail.com>
2026-05-24 16:51:06 +0700

    "Xây dựng tính năng đăng kí cho role Attendee"

---
+++ src/main/java/vn/edu/fpt/modelview/request/RegisterUserDTO.java
@@ -0,0 +1,32 @@
+package vn.edu.fpt.modelview.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Setter @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterUserDTO {
+    @NotBlank(message = "Họ tên không được để trống!")
+    private String fullName;
+    @NotBlank(message = "Số điện thoại không được để trống!")
+    private String phone;
+    @NotBlank(message = "Email không được để trống!")
+    @Email(message = "Email không hợp lệ")
+    private String username;
+    @NotBlank(message = "Vui lòng nhập mật khẩu")
+    private String password;
+    @NotBlank(message = "Confirm password không được để trống!")
+    private String confirmPassword;
+    private String gender;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dob;
+}
