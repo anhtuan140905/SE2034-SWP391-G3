9989d0b952af4843c2e47872a262107a759e00ae
TuanNPA040905 <tuannpahe190953@gmail.com>
2026-05-25 14:15:30 +0700

    "Fix lỗi thiếu registerOrgDTO khi import"

---
+++ src/main/java/vn/edu/fpt/modelview/request/RegisterOrgDTO.java
@@ -0,0 +1,38 @@
+package vn.edu.fpt.modelview.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterOrgDTO {
+    @NotBlank(message = "Họ tên không được để trống!")
+    private String fullName;
+    @NotBlank(message = "Số điện thoại không được để trống!")
+    private String phone;
+    @NotBlank(message = "Email không được để trống!")
+    @Email(message = "Email không hợp lệ")
+    private String username;
+    @NotBlank(message = "Vui lòng nhập mật khẩu")
+    private String password;
+    @NotBlank(message = "Confirm password không được để trống!")
+    private String confirmPassword;
+    private String gender;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dob;
+    @NotBlank(message = "Tên công ty không được để trống")
+    private String companyName;
+    @NotBlank(message = "Tax code không được để trống")
+    private String taxCode;
+    @NotBlank(message = "Bank account không được để trống")
+    private String bankAccount;
+}
