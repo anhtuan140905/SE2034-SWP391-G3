0c1516f79fb1968c0bef3258296ae652530269ca
HoangAnh916 <nha29052005@gmail.com>
2026-05-25 16:05:44 +0700

    controller Organizer

---
+++ src/main/java/vn/edu/fpt/controller/organizer/Organizer.java
@@ -0,0 +1,15 @@
+package vn.edu.fpt.controller.organizer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/organizer")
+public class Organizer {
+
+    @GetMapping //
+    public String getDashboard(){
+        return "organizer/DashboardOrganizer";
+    }
+}
