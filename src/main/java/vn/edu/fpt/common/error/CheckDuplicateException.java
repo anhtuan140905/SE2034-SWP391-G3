f256c524344b4cd70ef0667909b9259f8ae8da33
TuanNPA040905 <tuannpahe190953@gmail.com>
2026-05-29 22:39:23 +0700

    "Format Exception CheckDuplicateException"

--- src/main/java/vn/edu/fpt/common/error/CheckDuplicateException.java
+++ src/main/java/vn/edu/fpt/common/error/CheckDuplicateException.java
@@ -3,5 +3,7 @@ package vn.edu.fpt.common.error;
 public class CheckDuplicateException extends RuntimeException{
     public CheckDuplicateException(String message){
         super(message);
+
+
     }
 }
