abb914bbf9d42d1a64383ee7c2ffb01a64ed0a5f
HoangAnh916 <nha29052005@gmail.com>
2026-05-30 23:03:26 +0700

    update screen Dashboard Organizer and controller Dashboard Organizer

--- src/main/java/vn/edu/fpt/controller/organizer/DashBoardController.java
+++ src/main/java/vn/edu/fpt/controller/organizer/DashBoardController.java
@@ -1,6 +1,7 @@
 package vn.edu.fpt.controller.organizer;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DashBoardController {
 
     @GetMapping //
-    public String getDashboard(){
+    public String dashboard(Model model) {
+        model.addAttribute("activeMenu", "dashboard");
         return "organizer/DashboardOrganizer";
     }
     @GetMapping("/create/event")
