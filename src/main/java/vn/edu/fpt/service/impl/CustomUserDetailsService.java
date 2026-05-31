7cfc4f5d08048b0b0357f55f27fe30649f776782
TuanNPA040905 <tuannpahe190953@gmail.com>
2026-05-31 20:17:46 +0700

    "Bổ sung config Spring Security"

---
+++ src/main/java/vn/edu/fpt/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package vn.edu.fpt.service.impl;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+import vn.edu.fpt.service.UserService;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        vn.edu.fpt.model.User user = this.userService.findByEmailWithRoles(username).get();
+        if(user == null){
+            throw new UsernameNotFoundException("Không tìm thấy USER");
+        }
+        return new CustomUserDetails(user);
+    }
+}
