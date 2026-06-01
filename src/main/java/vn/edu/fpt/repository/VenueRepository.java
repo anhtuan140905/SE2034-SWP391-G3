a1adacb99d175d0f7e0b68d1b1007d068801060d
HoangAnh916 <nha29052005@gmail.com>
2026-06-01 23:53:02 +0700

    update file VenueRepository

--- src/main/java/vn/edu/fpt/repository/VenueRepository.java
+++ src/main/java/vn/edu/fpt/repository/VenueRepository.java
@@ -1,9 +1,19 @@
 package vn.edu.fpt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.edu.fpt.model.Venue;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface VenueRepository extends JpaRepository<Venue,Long> {
+    @Query("SELECT v FROM Venue v WHERE v.id NOT IN (" +
+            "  SELECT e.venue.id FROM Event e " +
+            "  WHERE CAST(e.date AS date) = :targetDate" +
+            ")")
+    List<Venue> findAvailableVenuesByDate(@Param("targetDate") LocalDate targetDate);
 }
