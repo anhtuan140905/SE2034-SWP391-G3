3662c57b0f7b27cfc6a3054c75c920f82802e4d5
Nguyen Vu Hoang Phuc <cohphuc1234509876@gmail.com>
2026-07-04 01:41:09 +0700

    Thêm chức năng create voucher

---
+++ src/main/java/vn/edu/fpt/service/VoucherService.java
@@ -0,0 +1,14 @@
+package vn.edu.fpt.service;
+
+import vn.edu.fpt.model.Voucher;
+import vn.edu.fpt.modelview.request.organizer.CreateVoucherRequest;
+
+import java.util.List;
+
+public interface VoucherService {
+    void createVoucher(Long evenId, Long userId, CreateVoucherRequest request);
+
+    List<Voucher> getVoucherByEvenId(Long evenId);
+
+    Voucher getVoucherDetail(Long eventId, Long voucherId);
+}
